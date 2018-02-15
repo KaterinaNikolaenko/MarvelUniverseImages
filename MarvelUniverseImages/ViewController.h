b11511e927c4c7fa64ec309de1887bb7295ea38c
Katerina <katya-nik@list.ru>
2018-02-15 23:48:12 +0200

    Initial Commit

---
+++ MarvelUniverseImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MarvelUniverseImages
+//
+//  Created by Katerina on 15.02.18.
+//  Copyright © 2018 Katerina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MarvelUniverseImages/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MarvelUniverseImages
+//
+//  Created by Katerina on 15.02.18.
+//  Copyright © 2018 Katerina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
