1b65a727c3506d55f6c43441ecb374bfb7473302
Katerina <katya-nik@list.ru>
2018-02-16 17:41:09 +0200

    [+] created TableView

---
+++ MarvelUniverseImages/ViewControllers/MainViewController.m
@@ -0,0 +1,59 @@
+//
+//  ViewController.m
+//  MarvelUniverseImages
+//
+//  Created by Katerina on 15.02.18.
+//  Copyright © 2018 Katerina. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+{
+NSArray *name;
+}
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    name = [[NSArray alloc]initWithObjects:@"Mobiles", @"Costumes", @"Shoes", nil];
+    UIEdgeInsets inset = UIEdgeInsetsMake(20, 0, 0, 0);
+    
+    self.tableView.contentInset = inset;
+    self.tableView.scrollIndicatorInsets = inset;
+    
+    self.tableView.dataSource = self;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return name.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    cell.textLabel.text = [name objectAtIndex:indexPath.row];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%ld",(long)indexPath.row];
+
+    return cell;
+}
+
+@end
